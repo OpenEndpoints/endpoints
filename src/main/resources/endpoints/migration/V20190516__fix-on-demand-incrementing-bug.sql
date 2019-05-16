09f9a70842814de8f073c0a543c0841b9aa557de
Adrian Smith <adrian.m.smith@gmail.com>
2019-05-16 14:56:53 +0200

    Bug fix on-demand increment numbers

---
+++ src/main/resources/endpoints/migration/V20190516__fix-on-demand-incrementing-bug.sql
@@ -0,0 +1,3 @@
+ALTER TABLE request_log
+DROP CONSTRAINT request_log_application_environment_on_demand_month_increme_key,
+DROP CONSTRAINT request_log_application_environment_on_demand_year_incremen_key;
