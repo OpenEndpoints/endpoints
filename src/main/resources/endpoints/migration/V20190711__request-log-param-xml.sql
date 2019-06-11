3b49cf6ed7df0fe895e10dc1818b8d42dd698500
Adrian Smith <adrian.m.smith@gmail.com>
2019-06-11 12:56:39 +0200

    Add "request_log" params input/output xml, and jOOQ binding to use them

---
+++ src/main/resources/endpoints/migration/V20190711__request-log-param-xml.sql
@@ -0,0 +1,3 @@
+ALTER TABLE request_log
+ADD parameter_transformation_input XML NULL,
+ADD parameter_transformation_output XML NULL;
---
+++ src/test/java/com/databasesandlife/util/jooq/PostgresXmlDomElementBindingTest.java
@@ -0,0 +1,41 @@
+package com.databasesandlife.util.jooq;
+
+import com.databasesandlife.util.DomParser;
+import com.databasesandlife.util.gwtsafe.ConfigurationException;
+import endpoints.DeploymentParameters;
+import endpoints.PublishEnvironment;
+import endpoints.config.ApplicationName;
+import endpoints.config.NodeName;
+import endpoints.generated.jooq.tables.records.RequestLogRecord;
+import junit.framework.TestCase;
+import lombok.SneakyThrows;
+import org.apache.commons.lang.RandomStringUtils;
+
+import java.time.Instant;
+
+import static endpoints.generated.jooq.Tables.REQUEST_LOG;
+
+public class PostgresXmlDomElementBindingTest extends TestCase {
+    
+    @SneakyThrows(ConfigurationException.class)
+    public void test() {
+        try (var tx = DeploymentParameters.get().newDbTransaction()) {
+            var userAgent = RandomStringUtils.randomAlphanumeric(50);
+            
+            var toInsert = new RequestLogRecord();
+            toInsert.setApplication(new ApplicationName("foo"));
+            toInsert.setEndpoint(new NodeName("foo"));
+            toInsert.setDatetimeUtc(Instant.now());
+            toInsert.setStatusCode(200);
+            toInsert.setUserAgent(userAgent);
+            toInsert.setEnvironment(PublishEnvironment.live);
+            toInsert.setParameterTransformationInput(DomParser.from("<input/>"));
+            toInsert.setParameterTransformationOutput(null);
+            tx.insert(toInsert);
+
+            var found = tx.jooq().selectFrom(REQUEST_LOG).where(REQUEST_LOG.USER_AGENT.eq(userAgent)).fetchOne();
+            assertEquals("input", found.getParameterTransformationInput().getNodeName());
+            assertNull(found.getParameterTransformationOutput());
+        }
+    }
+}
