65ab9b2db320d6243734cdd623c237cbbd240669
Adrian Smith <adrian.m.smith@gmail.com>
2020-04-07 11:27:48 +0200

    Improve code locality of DataSource XML parsing

--- src/main/java/endpoints/datasource/DataSource.java
+++ src/main/java/endpoints/datasource/DataSource.java
@@ -2,20 +2,39 @@ package endpoints.datasource;
 
 import com.databasesandlife.util.DomParser;
 import com.databasesandlife.util.gwtsafe.ConfigurationException;
+import com.databasesandlife.util.jdbc.DbTransaction;
+import com.offerready.xslt.WeaklyCachedXsltTransformer;
 import endpoints.TransformationContext;
 import endpoints.config.IntermediateValueName;
 import endpoints.config.ParameterName;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import javax.annotation.Nonnull;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
 
+import static com.databasesandlife.util.DomParser.getSubElements;
+
 public class DataSource {
     
-    public final @Nonnull List<DataSourceCommand> commands = new ArrayList<>();
+    protected final @Nonnull List<DataSourceCommand> commands = new ArrayList<>();
+
+    public DataSource() { }
+
+    public DataSource(
+        @Nonnull DbTransaction tx, @Nonnull WeaklyCachedXsltTransformer.XsltCompilationThreads threads,
+        @Nonnull File applicationDir, @Nonnull File httpXsltDirectory, @Nonnull File xmlFromApplicationDir, @Nonnull Element script
+    ) throws ConfigurationException {
+        if ( ! script.getTagName().equals("data-source")) throw new ConfigurationException("Data source should have root tag " +
+            "<data-source> but instead has <"+script.getTagName()+">");
+        for (var command : getSubElements(script, "*"))
+            commands.add(DataSourceCommand.newForConfig(tx, threads,
+                applicationDir, httpXsltDirectory, xmlFromApplicationDir, command));
+    }
 
     /** Checks that no variables other than those supplied are necessary to execute all commands */
     public void assertParametersSuffice(
